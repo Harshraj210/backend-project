4815dd832ec3d2c796f3ee6efc24a1152242bceb
harsh <nh1750501@gmail.com>
2025-10-27 23:50:16 +0530

    Updated hasing password

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,16 +2,21 @@ import User from '../models/userModel';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
+dotenv.config()
 
 const handleRegister =  async(req, res)=>{
   const {name,email,password}= req.body
   if(!name || !email || !password){
-    return res.status(404).json({message:"Signup failed"})
+    return res.status(404).json({message:"Fill all the details Signup failed"})
   }
+  const userExist = User.findOne({email})
+  if(userExist){
+    return res.status(400).json({message:"USer already exist"})
+  }
+  const hashedPassword = await bcrypt(password,10)
   const newUser = await User.create({
     name,
     email,
-    password
+    password:hashedPassword
   })
 }

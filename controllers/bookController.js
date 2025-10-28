b75d1cb0b70e530009ef62c82fafc770e82833b5
harsh <nh1750501@gmail.com>
2025-10-28 22:03:23 +0530

    added finding books functions

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,11 +1,48 @@
-import Book from "../models/bookModel";
-import User from "../models/userModel";
+import Book from '../models/bookModel';
+import User from '../models/userModel';
 
-const addBook = async(resizeBy,res)=>{
-  const{title,author,publisher,availablebooks}=req.body
-  if(!title || !author || !publisher || !availablebooks ){
-    return res.status(401).json({message:"All things are required"})
+const addBook = async (resizeBy, res) => {
+  try {
+    const {title, author, publisher, availablebooks, isbn} = req.body;
+    if (!title || !author || !publisher || !availablebooks) {
+      return res.status(401).json({message: 'All things are required'});
+    }
+    const bookexist = await Book.findOne({isbn});
+    if (bookexist) {
+      return res
+        .status(401)
+        .json({message: 'Book with this ISBN already exist'});
+    }
+    const newBook = await Book.create({
+      title,
+      author,
+      publisher,
+      genre,
+      isbn,
+      availablebooks,
+    });
+  } catch (error) {
+    return res.status(401).json({message: 'Book addition failed'});
   }
-  
+};
 
-}
+const getallBooks = async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({message: 'all books are here'}, books);
+  } catch (error) {
+    return res.status(401).json({message: 'Error in getting all books'});
+  }
+};
+const getbooksId = async (req, res) => {
+  try {
+    const books = await Book.findById(req.params.id);
+    if (!books) {
+      return res.status(401).json({message: 'book not found'});
+    }
+  } catch (error) {
+    return res
+      .status(401)
+      .json({message: 'Error in finding the required Book'});
+  }
+};

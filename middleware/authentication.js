41f9f5f513681e4c1a1c9ba322a9deb2daf302a5
harsh <nh1750501@gmail.com>
2025-10-29 12:41:31 +0530

    updated middleware file

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -7,5 +7,23 @@ dotenv.config()
 
 
 const protect = async(req,resizeBy,next)=>{
-  
+  try {
+    const authHeader = req.header.authorization
+
+    if (!authHeader || !authHeader.startsWith( "Bearer ")) {
+      return res.status(401).json({ message: "No token, Not authorized!!" });
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    req.user = await User.findById(decoded.id).select("-password");
+      if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    next();
+  } catch (error) {
+     return res.status(401).json({ message: "Token invalid!!" });
+  }
 }
+
+export default protect;
